ff2ec418f1743fad1593c49493da9c6621f20966
Emanuel Lewis <emanuellewis51@gmail.com>
2026-05-17 23:29:05 -0400

    Commenst deleted

--- components/Todo.js
+++ components/Todo.js
@@ -54,20 +54,3 @@ class Todo {
 }
 
 export default Todo;
-
-//   const todoElement = todoTemplate.content
-//     .querySelector('.todo')
-//     .cloneNode(true);
-//   const todoNameEl = todoElement.querySelector('.todo__name');
-//   const todoCheckboxEl = todoElement.querySelector('.todo__completed');
-//   const todoLabel = todoElement.querySelector('.todo__label');
-//   const todoDate = todoElement.querySelector('.todo__date');
-//
-
-//  // _generateId() {
-//   if (!this._data.id) {
-//     this._data.id = uuidv4();
-//   }
-// this._generateId();
-// }
-//   });
